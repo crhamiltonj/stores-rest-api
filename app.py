bc31329e96483972d94427199f8f3443e76fb69d
Charles Hamilton <crhamiltonj@gmail.com>
2019-08-05 14:19:28 -0400

    Added run.py for database setup

--- app.py
+++ app.py
@@ -13,11 +13,6 @@ app.secret_key = "this is a secret"
 api = Api(app)
 
 
-@app.before_first_request
-def create_tables():
-    db.create_all()
-
-
 jwt = JWT(app, authenticate, identity)
 
 api.add_resource(Item, "/item/<string:name>")
